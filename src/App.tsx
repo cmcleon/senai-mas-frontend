86608715f907d4d1f1b9d9c530a9fe86a4b87bef
cmcleon <candicem.leon@gmail.com>
2021-10-26 20:45:09 -0300

    :package:  estilo global do proj

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {BrowserRouterProps as Router} from 'react-router-dom';
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-  <div className="App">
-  <h1>Frontend My Activies Space </h1>
-  </div>
-  );
+    <Router>
+      <h1>Frontend My Activies Space </h1>
+      <GlobalStyle />
+    </Router>
+    );
   }
   export default App;
